fe0d19a9e4297c951300609818a93bed316ab6f5
NotChr1s <pollohijo65@gmail.com>
2024-04-15 21:38:48 -0600

    Se agrega el proyecto

---
+++ ClosetWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ClosetWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClosetWorld extends World
+{
+
+    /**
+     * Constructor for objects of class ClosetWorld.
+     * 
+     */
+    public ClosetWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
