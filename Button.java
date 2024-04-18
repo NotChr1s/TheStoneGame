e0116648f2db26e460a7073d8623985c336e8b3a
NotChr1s <pollohijo65@gmail.com>
2024-04-17 23:16:44 -0600

    main selected buttons animation

--- Button.java
+++ Button.java
@@ -9,21 +9,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 class Button extends Actor
 {
     private GreenfootImage imagenNormal;
+    private GreenfootImage imagenSeleccion;
     private World newWorld;
     
-    public Button(String imageButton, World newWorld) {
+    public Button(String imageButton, String imageSelectButton, World newWorld) {
         imagenNormal = new GreenfootImage(imageButton);
+        imagenSeleccion = new GreenfootImage(imageSelectButton);
         setImage(imagenNormal);
         this.newWorld= newWorld;
     }
     
     public Button() {
         imagenNormal = new GreenfootImage("BasicButton.png");
+        imagenSeleccion = new GreenfootImage("SelectedBasicButton.png");
         setImage(imagenNormal);
         this.newWorld= new InitialWorld();
     }
     
     public void act() {
+        if (Greenfoot.mouseMoved(this)) {  
+            setImage(imagenSeleccion);  
+        } 
+        if (Greenfoot.mouseMoved(null) && !Greenfoot.mouseMoved(this)) {  
+            setImage(imagenNormal);  
+        }
         if (Greenfoot.mouseClicked(this)) {
             Greenfoot.setWorld(newWorld);
         }
--- InitialWorld.java
+++ InitialWorld.java
@@ -19,26 +19,31 @@ public class InitialWorld extends World
         prepare();
     }
     
+    private void prepareButtons()
+    {
+        World world = new CombatWorld();
+        Button button1 = new Button("CombatButton.png","SelectedCombatButton.png",world);
+        addObject(button1,540,350);
+        World world2 = new ClosetWorld();
+        Button button2 = new Button("ClosetButton.png","SelectedClosetButton.png",world2);
+        addObject(button2,270,550);
+        World world3 = new ShopWorld();
+        Button button3 = new Button("ShopButton.png","SelectedShopButton.png",world3);
+        addObject(button3,810,550);
+        World world4 = new GachaWorld();
+        Button button4 = new Button("GachaButton.png","SelectedGachaButton.png",world4);
+        addObject(button4,540,600);
+        World world5 = new CombatWorld();
+        Button button5 = new Button("SettingsButton.png","SelectedSettingsButton.png",world5);
+        addObject(button5,50,670);
+    }
+    
     private void prepare()
     {
         GreenfootImage nuevaImagenFondo = new GreenfootImage("ProvisionalBG2.jpg");
         setBackground(nuevaImagenFondo);
         GameTitle gametitle = new GameTitle();
         addObject(gametitle,540,150);
-        World world = new CombatWorld();
-        Button button1 = new Button("CombatButton.png",world);
-        addObject(button1,540,350);
-        World world2 = new ClosetWorld();
-        Button button2 = new Button("ClosetButton.png",world2);
-        addObject(button2,270,550);
-        World world3 = new ShopWorld();
-        Button button3 = new Button("ShopButton.png",world3);
-        addObject(button3,810,550);
-        World world4 = new GachaWorld();
-        Button button4 = new Button("GachaButton.png",world4);
-        addObject(button4,540,600);
-        World world5 = new CombatWorld();
-        Button button5 = new Button("SettingsButton.png",world5);
-        addObject(button5,50,670);
+        prepareButtons();
     }
 }
